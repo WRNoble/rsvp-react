c0a125f67d6cd5f78eebc1bcbef74041b03cbbee
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-09-18 11:01:31 -0400

    added toggle confirmation method

--- src/GuestList.js
+++ src/GuestList.js
@@ -1,1 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Guest from "./Guest";
+
+const GuestList = (props) => (
+  <ul>
+    {props.guests.map((guests, index) => (
+      <Guest key={index} name={guests.name} isConfirmed={guests.isConfirmed} />
+    ))}
+  </ul>
+);
